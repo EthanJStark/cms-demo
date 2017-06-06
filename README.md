a865cf5b0edab378448b7bc6bc54a0fc4dad57b7
Ethan Stark <ethanstark@gmail.com>
2017-06-06 09:56:13 -0700

    Wrapping up, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # cms-demo
 Demo of content management system
+
+# Installation
+
+Prerequisites: node, PostgreSQL
+
+`git clone https://github.com/EthanJStark/cms-demo.git` - Clone the repo
+
+`createdb cms-demo` - Create the database
+
+`npm i` - Install dependencies
+
+Correct the path to the database: find the two instances of `estark` and replace them with the result of typing `whoami` in your terminal.
+
+`npm run migrate:latest` - run migrations
+
+User must manually populate the database
+
+## Schema
+![Alt](cms-schema.png "Schema")
--- database/media.js
+++ database/media.js
@@ -1,8 +1,11 @@
 const db = require( './db' )
 
-const getMediaForPostId = `select media.* from media join posts_media
-  on media.id = posts_media.media_id where posts_media.post_id=$1`
+const getMediaForPostId = `SELECT media.* FROM media
+  JOIN posts_media ON media.id = posts_media.media_id WHERE posts_media.post_id=$1`
+
+const getAll = `SELECT * FROM media`
 
 module.exports = {
-  getMediaForPostId: postId => db.any( getMediaForPostId, postId )
+  getMediaForPostId: postId => db.any( getMediaForPostId, postId ),
+  getAll: () => db.any( getAll )
 }
